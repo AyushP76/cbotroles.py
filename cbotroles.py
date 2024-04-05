c2430009477cc5a63f427a1c108477df55bdfff7
AyushP76 <128037431+AyushP76@users.noreply.github.com>
2024-04-05 21:42:26 +0530

    Update cbotroles.py

--- cbotroles.py
+++ cbotroles.py
@@ -14,12 +14,11 @@ from langchain.text_splitter import RecursiveCharacterTextSplit
 # nltk.download('wordnet') 
 # nltk.download('omw-1.4'
 
-sent_tokens = []
-word_tokens = []
+sent_tokens= []
+word_tokens= []
 
 GREETING_INPUTS = ("hello", "yo", "hi", "greetings", "sup", "what's up", "wassup", "hey", "namaste")
 GREETING_RESPONSES = ["hi", "nice to meet you..", "hey", "*nods*", "hi there", "hello", 'yes..?', 'greetings..']
-
 
 # Checking for greetings
 def greeting(sentence):
@@ -30,52 +29,49 @@ def greeting(sentence):
 
 # Generating response
 def response(user_response):
-    robo_response = ''
+    robo_response=''
     sent_tokens.append(user_response)
     TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
     tfidf = TfidfVec.fit_transform(sent_tokens)
     vals = cosine_similarity(tfidf[-1], tfidf)
-    idx = vals.argsort()[0][-2]
+    idx=vals.argsort()[0][-2]
     flat = vals.flatten()
     flat.sort()
     req_tfidf = flat[-2]
-    if (req_tfidf == 0):
-        robo_response = robo_response + "Sorry, no data available for this."
+    if(req_tfidf==0):
+        robo_response=robo_response+"Sorry, no data available for this."
         return robo_response
     else:
-        robo_response = robo_response + sent_tokens[idx]
+        robo_response = robo_response+sent_tokens[idx]
         return robo_response
 
-
-# Lemmatizing the data
+#Lemmatizing the data
 def LemTokens(tokens):
     lemmer = nltk.stem.WordNetLemmatizer()
     return [lemmer.lemmatize(token) for token in tokens]
 
-
-# Normalising data by removing punctuations
+#Normalising data by removing punctuations
 def LemNormalize(text):
     remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
     return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
 
-
-# Main Method
+#Main Method
 def main():
-    flag = True
-    print("\nEnter the role you are asking for:")
+    flag=True
+    print("\nHi, Enter the role you are asking for. Type 'Bye' to exit. Type 'New' to start new topic queries.")
     print("******************************************************************")
-    topic = str(input("Please enter the question about the role you want to ask queries for: "))
+    topic=str(input("Please enter the topic you want to ask queries for: "))
     print("\nCollecting data...")
     print("******************")
 
-    # Fetching data from wikipedia
+    #Fetching data from wikipedia
     try:
-        raw = page(topic)
-        raw = raw.content
-        raw = raw.lower()
+        raw=page(topic)
+        raw=raw.content
+        raw=raw.lower()
 
     except:
-        print("Sorry no wikipedia data available for: ", topic + ". Try Again!!!")
+        print("Sorry no wikipedia data available for: ",topic + ". Try Again!!!")
         main()
 
     global sent_tokens
@@ -86,29 +82,28 @@ def main():
 
     print("Data Fetched. Ready to go...")
 
-    while (flag == True):
+    while(flag==True):
         user_response = input("You: ")
-        user_response = user_response.lower().strip()
-        if (user_response != 'bye' and user_response != 'new'):
-            if (user_response in ['thanks', 'thank you', 'thanx', 'thnx']):
-                flag = False
+        user_response=user_response.lower().strip()
+        if(user_response!='bye' and user_response!='new'):
+            if(user_response in ['thanks', 'thank you', 'thanx', 'thnx']):
+                flag=False
                 print("Wiki: You are welcome.")
             else:
-                if (greeting(user_response) != None):
-                    print("Wiki: " + greeting(user_response))
+                if(greeting(user_response)!=None):
+                    print("Wiki: "+greeting(user_response))
                 else:
-                    print("Wiki: ", end="")
+                    print("Wiki: ",end="")
                     print(response(user_response))
                     sent_tokens.remove(user_response)
 
-        elif (user_response == 'new'):
+        elif(user_response=='new'):
             main()
 
         else:
-            flag = False
+            flag=False
             print("Wiki: Bye!!! See you soon.. :) ")
             break
-
 
 if __name__ == '__main__':
     main()
